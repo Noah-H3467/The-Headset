513fc7e39a20483ddc42aec580921a1ed7f3646d
Noah-H3467 <nhaskell26@team3467.org>
2026-01-25 17:38:06 -0500

    GNU GPL header on main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
 /** 
  * Copyright (C) 2026 Noah Haskell
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <https://www.gnu.org/licenses/>.
+ * 
  * File: main.cpp
  * Author: Noah Haskell
  * Program Description:
  * "The Headset" is currently a prototype to automate the application of a cold compress on the wearer's eyes. 
  * This is intended to treat eye itching and pain caused by allergies.
- * This is the main code file for the Headset.
+ * This is the main code file for the Headset. Its key role is to contain the high level logic.
 */
 
 #include <Arduino.h>
@@ -37,7 +49,7 @@
     SOFTWARE.
 
   https://github.com/curiores/ArduinoTutorials/blob/main/MultipleEncoders/SimplePositionPID/SimplePositionPID.ino */
-  
+
 class SimplePID{
   private:
     float kp, kd, ki, ks, umax; // Parameters
